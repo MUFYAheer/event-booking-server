7fd5db58dfb800b8e5683997675266dbb7dd3425
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2019-01-29 11:39:31 +0500

    Add Schemas & Resolvers in /graphql

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const logger = require('morgan');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use('/', (req, res) => res.send('Its working!'));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery,
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => ['Cooking', 'Fishing', 'All-night coding'],
+      createEvent: args => args.name,
+    },
+    graphiql: true,
+  }),
+);
 
 module.exports = app;
