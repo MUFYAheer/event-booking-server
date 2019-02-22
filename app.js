cbc686487840007dd73a4dbcd05102869e3304c0
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2019-02-22 17:57:27 +0500

    Add cross-origin

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  return req.method === 'OPTIONS' ? res.sendStatus(200) : next();
+});
+
 app.use(authMiddleware);
 
 app.use(
